3b4e5edd66ff24b178bdfd76b01ef6491f8779ac
unknown <kapilyadav9127@gmail.com>
2022-09-11 13:29:05 +0530

    first deploy

--- streamlater/settings.py
+++ streamlater/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default= False,cast = bool)
 
-ALLOWED_HOSTS = ['10.0.2.2' ]
+ALLOWED_HOSTS = ['https://streamlater.herokuapp.com/' ]
 
 
 # Application definition
